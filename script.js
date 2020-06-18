f3d113dd99ab45fac5ec03a5e11a55bd992f2280
Nagy-Bana Éva <nagybanaeva@gmail.com>
2020-06-18 23:34:21 +0200

    add files

---
+++ script.js
@@ -0,0 +1,11 @@
+var brownie = {
+	'butter': '1/2 bögre', 
+	'sugar': '1 bögre', 
+	'unsweetened cocoa powder': '1 bögre', 
+	'eggs': '2 nagy darab', 
+	'flour': '1/2 bögre',
+	'things to do': function () {
+	document.write('Mix all the ingredients in a large bowl!');
+}
+};
+
